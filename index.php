4da5055dd2c2501ed0ab9ce0a2b66be6a00777ce
dayujunior <112327277+dayujunior@users.noreply.github.com>
2022-08-29 14:02:06 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,303 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" href="img/icon.jpg">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	
+	<!-- boostrap icons -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.1/font/bootstrap-icons.css">
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+
+	<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+	
+	<!-- My CSS -->
+	<link rel="stylesheet" href="style.css">
+
+    <title>My Portfolio | Muhammad Dayu</title>
+  </head>
+  <body id="home">
+
+  <div class="container"></div>
+  
+	<!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-sm" style="background-color:#0c0033">
+	  <div class="container">
+		
+	  	<a class="nav-link active" aria-current="page" href="#home">
+			<button class="navbar-toggler" type="button" aria-expanded="false" aria-label="Toggle navigation">
+			<i class="bi bi-house-fill" style="color:#00ffff"></i>
+			</button>
+		</a>
+
+		<a class="nav-link" href="#about">
+			<button class="navbar-toggler" type="button" aria-expanded="false" aria-label="Toggle navigation">
+			<i class="bi bi-people-fill" style="color:#00ffff"></i>
+			</button>
+		</a>
+
+		<a class="nav-link" href="#galery">
+			<button class="navbar-toggler" type="button" aria-expanded="false" aria-label="Toggle navigation">
+			<i class="bi bi-image-fill" style="color:#00ffff"></i>
+			</button>
+		</a>
+
+		<a class="nav-link" href="#contact">
+			<button class="navbar-toggler" type="button"  aria-expanded="false" aria-label="Toggle navigation">
+			<i class="bi bi-person-rolodex" style="color:#00ffff"></i>
+			</button>
+		</a>
+		
+		<div class="collapse navbar-collapse" id="navbarNav">
+		<a class="navbar-brand" href="#home">
+			<img src="img/dayu.jpg" alt="Muhammad Dayu" width="30" height="30" class="rounded-circle" class="d-inline-block align-text-top">
+			Muhammad Dayu
+		</a>
+		  <ul class="navbar-nav ms-auto">
+			<li class="nav-item">
+			  <a class="nav-link active" aria-current="page" href="#home">Home</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#about">About</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#galery">Galery</a>
+			</li>
+			<li class="nav-item">
+			  <a class="nav-link" href="#contact">Contact</a>
+			</li>
+		  </ul>
+		</div>
+	  </div>
+	</nav>
+	<!-- akhir navbar -->
+	
+	<!-- jumbotron -->
+	<section class="jumbotron text-center">
+		<a href="#"><img src="img/dayu.jpg" alt="Muhammad Dayu" width="200" class="rounded-circle img-thumbnail bg" data-aos="flip-right" data-aos-easing="ease-out-cubic" data-aos-duration="2000"></a>
+		<h1 class="display-4" data-aos="zoom-in-up" data-aos-easing="ease-out-cubic" data-aos-duration="1000">Muhammad Dayu</h1>
+		<p class="lead" data-aos="zoom-in-up" data-aos-easing="ease-out-cubic" data-aos-duration="1500">My Hobi Is Play Football and Ngoding</p>
+		<a class="btn btn-lg contact" href="https://api.whatsapp.com/send?phone=6282346148469&text=Assalamualaikum%20Muhammad%20Dayu,%20bagaimana%20kabarnya?" role="button" target="_blank" data-aos="zoom-in-up" data-aos-easing="ease-out-cubic" data-aos-duration="2000">Contact Me</a>
+		
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,160L60,181.3C120,203,240,245,360,245.3C480,245,600,203,720,181.3C840,160,960,160,1080,176C1200,192,1320,224,1380,240L1440,256L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+		
+	</section>
+	<!-- akhir jumbotron -->
+	
+	<!-- about me -->
+	<section id="about">
+		<div class="container">
+			<div class="row text-center mb-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+				<div class="col">
+					<h2>About Me</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-5" data-aos="fade-right" data-aos-easing="ease-out-cubic" data-aos-duration="800">
+					<p style="text-align:justify">Mulai saat itu aku mulai introspeksi perihal diriku sendiri. Aku berusaha untuk menyelesaikan segala permasalahan yang menimpaku dengan hati yang lapang. Karena dengan begitu aku bisa menjadi bahagia. Aku juga tidak perlu membandingkan diriku dengan orang lain. Aku hanya perlu membandingkan diriku dengan aku yang kemarin. Maka dari itu aku ingin menjadi pribadi yang lebih baik.</p>
+					<p style="text-align:justify">Aku juga percaya jika setiap masalah yang menimpaku nantinya bisa menjadi pelajaran dalam hidupku. Karena selalu ada hikmah yang bisa aku ambil dari setiap suka dan duka ku. Yang membuat aku selalu yakin adalah setiap permasalahan ini datang dan dirancang oleh-Nya.</p>
+
+					<div class="d-inline-flex align-items-center mb-3">
+            			<a class="btn btn-sm btn-outline-primary btn-sm-square rounded-circle me-2" href="https://www.facebook.com/muhammad.dayu.146" target="blank"><i class="fab fa-facebook-f fw-normal" style="width:13px"></i></a>
+            			<a class="btn btn-sm btn-outline-success btn-sm-square rounded-circle me-2" href="https://api.whatsapp.com/send?phone=6282346148469&text=Assalamualaikum%20Muhammad%20Dayu,%20bagaimana%20kabarnya?" target="blank"><i class="fab fa-whatsapp fw-normal"></i></a>
+           				<a class="btn btn-sm btn-outline-danger btn-sm-square rounded-circle me-2" href="https://www.instagram.com/dayu.muhammad/" target="blank"><i class="fab fa-instagram fw-normal"></i></a>
+						<a class="btn btn-sm btn-outline-danger btn-sm-square rounded-circle" href="https://www.youtube.com/channel/UCefPnLL9BiAiPGshetZwFnw" target="blank"><i class="fab fa-youtube fw-normal" style="width:13px; text-align:center"></i></a>  
+         			</div>
+				</div>
+				<div class="col-md-5" data-aos="fade-left" data-aos-easing="ease-out-cubic" data-aos-duration="800">
+					<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+						<div class="carousel-inner">
+							<div class="carousel-item active">
+							<img src="img/about1.jpg" class="d-block w-100 img-thumbnail bg" alt="slide1">
+							</div>
+							<div class="carousel-item">
+							<img src="img/about2.jpg" class="d-block w-100 img-thumbnail bg" alt="slide2">
+							</div>
+							<div class="carousel-item">
+							<img src="img/about3.jpg" class="d-block w-100 img-thumbnail bg" alt="slide3">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0c0033" fill-opacity="1" d="M0,224L48,229.3C96,235,192,245,288,229.3C384,213,480,171,576,149.3C672,128,768,128,864,160C960,192,1056,256,1152,282.7C1248,309,1344,299,1392,293.3L1440,288L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+	</section>
+	<!-- akhir about me -->
+	
+	<!-- Galery -->
+	<section id="galery">
+		<div class="container">
+			<div class="row text-center mb-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+				<div class="col">
+					<h2>Galery</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center text-center mb-3">
+				<div class="col-md-1">
+					<a href="#">All</a>
+				</div>
+				<div class="col-md-1">
+					<a href="#">Foto</a>
+				</div>
+				<div class="col-md-1">
+					<a href="#">Video</a>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-4 mb-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+					<div class="card" data-tilt>
+					  <img src="img/galery1.jpg" class="card-img-top" alt="Galery 1">
+					  <div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn text-black contact">Selengkapnya</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4 mb-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+					<div class="card" data-tilt>
+					  <img src="img/galery2.jpg" class="card-img-top" alt="Galery 2">
+					  <div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn text-black contact">Selengkapnya</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4 mb-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+					<div class="card" data-tilt>
+					  <img src="img/galery3.jpg" class="card-img-top" alt="Galery 3">
+					  <div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn text-black contact">Selengkapnya</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4 mb-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+					<div class="card" data-tilt>
+					  <img src="img/galery2.jpg" class="card-img-top" alt="Galery 4">
+					  <div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn text-black contact">Selengkapnya</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4 mb-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+					<div class="card" data-tilt>
+					  <img src="img/galery3.jpg" class="card-img-top" alt="Galery 5">
+					  <div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn text-black contact">Selengkapnya</a>
+					  </div>
+					</div>
+				</div>
+				<div class="col-md-4 mb-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+					<div class="card" data-tilt>
+					  <img src="img/galery1.jpg" class="card-img-top" alt="Galery 6">
+					  <div class="card-body">
+						<h5 class="card-title">Card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						<a href="#" class="btn text-black contact">Selengkapnya</a>
+					  </div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#ffffff" fill-opacity="1" d="M0,224L48,208C96,192,192,160,288,176C384,192,480,256,576,240C672,224,768,128,864,112C960,96,1056,160,1152,192C1248,224,1344,224,1392,224L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+		
+	</section>
+	<!-- akhit galery -->
+	
+	<!-- contact -->
+	<section id="contact">
+		<div class="container">
+			<div class="row text-center mb-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+				<div class="col">
+					<h2>Contact</h2>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-md-5">
+					<form>
+						<div class="mb-3" data-aos="fade-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+							<label for="formGroupExampleInput" class="form-label">Nama</label>
+							<input type="text" class="form-control" id="formGroupExampleInput" placeholder="Masukkan Nama">
+						</div>
+						<div class="mb-3" data-aos="fade-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+							<label for="formGroupExampleInput2" class="form-label">Email</label>
+							<input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Masukkan Email">
+						</div>
+						<div class="mb-3" data-aos="fade-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+							<label for="formGroupExampleInput2" class="form-label">Subjek</label>
+							<input type="text" class="form-control" id="formGroupExampleInput3" placeholder="Masukkan Subjek">
+						</div>
+						<div class="mb-3" data-aos="fade-right" data-aos-easing="ease-out-cubic" data-aos-duration="1000">
+							<label for="formGroupExampleInput2" class="form-label">Pesan</label>
+							<textarea class="form-control" id="formGroupExampleInput4" placeholder="Masukkan Pesan Anda..."></textarea>
+						</div>
+						<div class="mb-3">
+							<div class="col-5">
+								<button type="reset" class="btn text-black contact me-3" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">Reset</button><button type="submit" class="btn text-black contact" data-aos="zoom-in" data-aos-easing="ease-out-cubic" data-aos-duration="1000">Kirim</button>
+							</div>
+						</div>
+					</form>
+				</div>
+				<div class="col-md-5">
+					<div class="mt-5">
+						<p  data-aos="fade-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000"><i class="fab fa-whatsapp fw-normal" style="color:green"></i> 082346148469</p>
+						<p  data-aos="fade-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000"><i class="fa fa-envelope" style="color:pink"></i> mdayujr@gmail.com</p>
+						<p  data-aos="fade-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000"><i class="fa fa-map-marker" style="color:red"></i> Jl. Haji Kalla panaikang makassar</p>
+					</div>
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.762755572648!2d119.45349061584933!3d-5.141852443221204!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee2d368ef2709%3A0xc7f301192252b55a!2sJl.%20H.%20Kalla%20No.2%2C%20Panaikang%2C%20Kec.%20Panakkukang%2C%20Kota%20Makassar%2C%20Sulawesi%20Selatan%2090231!5e0!3m2!1sid!2sid!4v1657962327752!5m2!1sid!2sid" class="w-100 h-80 mt-3 rounded" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"  data-aos="fade-left" data-aos-easing="ease-out-cubic" data-aos-duration="1000"></iframe>
+				</div>
+			</div>
+			<div class="text-center py-5">
+				<div class="container py-5">
+					<img src="img/client1.png" class="me-5 mb-5" alt="gojek" data-aos="flip-up" />
+					<img src="img/client2.png" class="me-5 mb-5" alt="grab" data-aos="flip-up"/>
+					<img src="img/client3.png" class="me-5 mb-5" alt="tokopedia" data-aos="flip-up"/>
+					<img src="img/client4.png" class="me-5 mb-5" alt="lazada" data-aos="flip-up"/>
+					<img src="img/client5.png" class="me-5 mb-5" alt="shopee" data-aos="flip-up"/>
+					<img src="img/client6.png" class="me-5 mb-5" alt="acer" data-aos="flip-up"/>
+					<img src="img/client7.png" class="me-5 mb-5" alt="asus" data-aos="flip-up"/>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- akhir contact -->
+	
+	<!-- footer -->
+	<footer class="text-white text-center pb-5 pt-5">
+		<p>Create with <i class="bi bi-heart-fill text-danger"></i> by <a href="https://www.instagram.com/dayu.muhammad/" class="text-white fw-bold" target="blank">Muhammad Dayu</a></p>
+		<div class="d-inline-flex align-items-center mb-3">
+            <a class="btn btn-sm btn-outline-primary btn-sm-square rounded-circle me-2" href="https://www.facebook.com/muhammad.dayu.146" target="blank"><i class="fab fa-facebook-f fw-normal" style="width:13px"></i></a>
+            <a class="btn btn-sm btn-outline-success btn-sm-square rounded-circle me-2" href="https://api.whatsapp.com/send?phone=6282346148469&text=Assalamualaikum%20Muhammad%20Dayu,%20bagaimana%20kabarnya?" target="blank"><i class="fab fa-whatsapp fw-normal"></i></a>
+           	<a class="btn btn-sm btn-outline-danger btn-sm-square rounded-circle me-2" href="https://www.instagram.com/dayu.muhammad/" target="blank"><i class="fab fa-instagram fw-normal"></i></a>
+			<a class="btn btn-sm btn-outline-danger btn-sm-square rounded-circle" href="https://www.youtube.com/channel/UCefPnLL9BiAiPGshetZwFnw" target="blank"><i class="fab fa-youtube fw-normal" style="width:13px; text-align:center"></i></a>  
+        </div>
+	</footer>
+	<!-- akhir footer -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+	<!-- animasi kursor -->
+	<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+	<script>
+  		AOS.init();
+	</script>
+
+	<!-- 3D -->
+	<script type="text/javascript" src="asset/vanilla-tilt.min.js"></script>
+
+  </body>
+</html>
